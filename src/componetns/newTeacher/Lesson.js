230d1bf359faeb2c606fa24521c961805ecdeff6
Dovudhon <dovudkhonhusanov5007@gmail.com>
2022-06-03 09:08:23 +0300

    add ot

--- src/componetns/newTeacher/Lesson.js
+++ src/componetns/newTeacher/Lesson.js
@@ -7,9 +7,6 @@ function Lesson() {
   return ( 
       <>
         <h3>Count : {count}</h3>
-        <h1>Alxamdulillax</h1>
-        <p>Zooor</p>
-          <p>salom</p>
         <button className='btn btn-success' onClick={() => setCount(count + 1)}>Incr</button>
         <button className='btn btn-danger' onClick={() => setCount(count - 1)}>Decr</button>
         <button className='btn btn-primary' onClick={() => setCount(0)}>cls</button>
