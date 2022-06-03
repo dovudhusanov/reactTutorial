6f3d5adc4a0d03883d56dd1607fcbc3b3d4c6d74
developer <developer@gmail.com>
2022-06-03 11:47:44 +0500

    ---ds

--- src/componetns/newTeacher/Lesson.js
+++ src/componetns/newTeacher/Lesson.js
@@ -7,9 +7,7 @@ function Lesson() {
   return ( 
       <>
           <div className='app w-50 mx-auto'>
-              <h1>toshoq</h1>
             <h3>Count : {count}</h3>
-            <p>bot</p>
               <button className='btn btn-success' onClick={() => setCount(count + 1)}>Incr</button>
               <button className='btn btn-danger' onClick={() => setCount(count - 1)}>Decr</button>
               <button className='btn btn-primary' onClick={() => setCount(0)}>cls</button>
