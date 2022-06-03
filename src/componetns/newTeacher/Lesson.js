93c3096c49c833be3dfbd5d58e9a1cf8b535752f
developer <developer@gmail.com>
2022-06-03 08:52:13 +0500

    zoor

--- src/componetns/newTeacher/Lesson.js
+++ src/componetns/newTeacher/Lesson.js
@@ -8,6 +8,7 @@ function Lesson() {
       <>
         <h3>Count : {count}</h3>
         <h1>Alxamdulillax</h1>
+        <p>Zooor</p>
           <p>salom</p>
         <button className='btn btn-success' onClick={() => setCount(count + 1)}>Incr</button>
         <button className='btn btn-danger' onClick={() => setCount(count - 1)}>Decr</button>
