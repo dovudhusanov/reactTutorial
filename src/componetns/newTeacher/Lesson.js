5a69d8e598c50a489553bc34f2c43e2d3625d310
Dovudhon <dovudkhonhusanov5007@gmail.com>
2022-06-03 10:16:24 +0300

    -bot

--- src/componetns/newTeacher/Lesson.js
+++ src/componetns/newTeacher/Lesson.js
@@ -8,7 +8,6 @@ function Lesson() {
       <>
           <div className='app w-50 mx-auto'>
             <h3>Count : {count}</h3>
-            <lata>Abdulbosit lata</lata>
               <button className='btn btn-success' onClick={() => setCount(count + 1)}>Incr</button>
               <button className='btn btn-danger' onClick={() => setCount(count - 1)}>Decr</button>
               <button className='btn btn-primary' onClick={() => setCount(0)}>cls</button>
