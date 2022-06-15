28d4557b593e68f83d21c1519a56f5894fb58c32
Dovudhon <dovudkhonhusanov5007@gmail.com>
2022-06-15 13:05:12 +0300

    allll

--- src/componetns/newTeacher/ReactHooks/UseState.js
+++ src/componetns/newTeacher/ReactHooks/UseState.js
@@ -3,15 +3,24 @@ import './Hook.css'
 
 function UseState(props) {
 
-    const [count, setCount] = useState(0)
+    function RandomNum () {
+        return Math.trunc(Math.random() * 20)
+    }
+
+    const [count, setCount] = useState(RandomNum())
+    function inc() {
+        console.log('Calculate...')
+        setCount(prev => prev + 1)
+    }
 
     return (
         <div className=' container d-flex justify-content-center mt-5'>
             <div className='group'>
                 <h1 className='text-center'>Count: {count}</h1>
                 <div>
-                    <button className='btn btn-success' onClick={() => setCount(count + 1)}>Add</button>
-                    <button className='btn btn-danger' onClick={() => setCount(count - 1)}>Remove</button>
+                    <button className='btn btn-success' onClick={inc}>Add</button>
+                    <button className='btn btn-danger' onClick={() => setCount(count - 1)}>Dec</button>
+                    <button className='btn btn-danger' onClick={() => setCount(0)}>Remove</button>
                 </div>
             </div>
         </div>
