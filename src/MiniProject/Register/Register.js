d8a46ee7db6bd22c3319dddd3b7fc94135d74095
Dovudhon <dovudkhonhusanov5007@gmail.com>
2022-09-24 09:51:07 +0500

    validation

--- src/MiniProject/Register/Register.js
+++ src/MiniProject/Register/Register.js
@@ -1,8 +1,18 @@
-import {useContext, useEffect, useState} from "react";
+import {useContext, useEffect, useRef, useState} from "react";
 import "../Login/Login.css";
 import {Link} from "react-router-dom";
 
+const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*{!@#$%]).{8,24}$/;
+
 const Register = () => {
+
+    const userRef = useRef()
+    const errRef = useRef()
+
+    const [user, serUser] = useState('');
+    const [validName, setValidName] = useState(false);
+    const [userFocus, setUserFocus] = useState(false)
 
     const [value, setValue] = useState({
         name: "",
